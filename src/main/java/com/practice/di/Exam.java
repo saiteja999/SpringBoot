4d290cc084f4e5943acd29b99c6c187f212e69cd
info2214 <info@cric-indo.com>
2024-04-04 22:03:28 -0400

    Autowiring - byName - Invigilator and Student

--- src/main/java/com/practice/di/Exam.java
+++ src/main/java/com/practice/di/Exam.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Exam {
     private Student student;
-
-    public Exam(){}
-
-    public Exam(Student student){
+    private Invigilator invigilator;
+    public void setStudent(Student student) {
         this.student = student;
     }
 
+    public void setInvigilator(Invigilator invigilator) {
+        this.invigilator = invigilator;
+    }
     public void displayExamInfo(){
         student.displayStudentInfo();
+        invigilator.displayInfo();
     }
 }
