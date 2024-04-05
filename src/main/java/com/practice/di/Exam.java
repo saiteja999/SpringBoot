018e14c24785a6245a6730d703cb09512597667d
info2214 <info@cric-indo.com>
2024-04-04 23:02:18 -0400

    Using @Autowired in class -  <context:annotation-config>

--- src/main/java/com/practice/di/Exam.java
+++ src/main/java/com/practice/di/Exam.java
@@ -5,7 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Exam {
     private Student student;
     private Invigilator invigilator;
-    public void setStudent(Student student) {
+
+//    public void setStudent(Student student) {
+//        this.student = student;
+//    }
+
+    public Exam(){}
+
+    @Autowired
+    public Exam(Student student){
         this.student = student;
     }
 
